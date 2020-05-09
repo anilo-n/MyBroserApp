58ad1d948bf2f3a4bc8c0d406094ece97a76a690
Apple <apple@ApplenoMBP.local>
2020-05-09 17:25:07 +0900

    早くみろ

---
+++ app/src/main/java/com/example/mybroserapp/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.mybroserapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView myWebView;
+    private EditText urlText;
+
+    private static final String INITIAL_WEBSITE = "http://dotinstall.com";
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myWebView = (WebView) findViewById(R.id.myWebView);
+        urlText = (EditText) findViewById(R.id.urlText);
+
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                getSupportActionBar().setSubtitle(view.getTitle());
+                urlText.setText(url);
+            }
+        });
+        myWebView.loadUrl(INITIAL_WEBSITE);
+    }
+}
